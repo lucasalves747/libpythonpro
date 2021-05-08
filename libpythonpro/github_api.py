21f890befcd5487fc4e4d580aba583b1154675ea
MICRO <134sandrinha@gmail.com>
2021-05-07 22:32:58 -0300

    colocado os distintivos

--- libpythonpro/github_api.py
+++ libpythonpro/github_api.py
@@ -1,17 +1,17 @@
 import requests
 
-
-def buscar_avatar(usuario):
-
-    """
+"""
     busca um avatar de usuarios  no github
     :param usuario: str com nome de usuario no Github
     :return:str com o link do avatar
     """
+def procurar_avatar(usuario):
     url = f'https://api.github.com/users/{usuario}'
     resp = requests.get(url)
-    return resp.json()['avatar_url']
+    return resp.json()[ 'avatar_url']
+
 
 
 if __name__ == '__main__':
-    print(buscar_avatar('lucasalves747'))
+    print(procurar_avatar('lucasAlves747'))
+
